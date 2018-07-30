de015dedeb8054836cc40b5480571a1c1db3ae4b
Francisco Braga <franciscobragafernandes@gmail.com>
2018-07-30 17:28:46 -0300

    In the beggining of styling the show page

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flats.each do |owner, flat|
+  proxy "/flats/#{flat.owner}.html", "/flats/show.html", locals: { owner: flat.owner }, ignore: true
+end
